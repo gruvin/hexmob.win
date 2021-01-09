a452530f6cdade96dfa0366c138d81eab21d6bed
Bryan Rentoul <gruvin@gmail.com>
2021-01-10 00:41:39 +1300

    removed comment block

--- src/StakeInfo.js
+++ src/StakeInfo.js
@@ -45,15 +45,6 @@ export class StakeInfo extends React.Component {
         const daysServed = Math.min(currentDay - stake.startDay, stake.stakedDays)
         const _endDate = new Date(HEX.START_DATE.getTime() + endDay * 24 * 3600 * 1000)
         const endDate = _endDate.toLocaleDateString()+' '+_endDate.toLocaleTimeString()
-        /*
-        const percentAPY = currentDay < stake.startDay 
-            ? 0 
-            : ( currentDay < stake.endDay 
-                ? 365 / Math.min(daysServed, 365) * percentGain
-                : percentGain / 365
-            )
-            */
-
         const percentAPY = new BigNumber(365).div(daysServed).times(percentGain)
 
         const pending = (currentDay < stake.lockedDay)
