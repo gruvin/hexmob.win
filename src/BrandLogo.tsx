27eecb132b09d813598184f97a34d843ec2d3a89
Bryan Rentoul <gruvin@gmail.com>
2023-07-04 18:23:20 +1200

    clean for wagmi rewrite

--- src/BrandLogo.tsx
+++
@@ -1,8 +0,0 @@
-export default () => {
-    return (
-        <div id="branding">
-            <h1 id="header_logo">GO<sup className="text-muted small"> .tshare.app</sup></h1>
-        </div>
-
-    )   
-}
