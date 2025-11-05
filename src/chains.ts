ae31ca3582b941448879a6a06fad6b2a155b083b
Bryan J. Rentoul <gruvin@gmail.com>
2025-11-05 14:49:41 +1300

    consolidated code for custom chain avatars

--- src/chains.ts
+++ src/chains.ts
@@ -1,6 +1,7 @@
 export interface TChain {
     description: string
     name: string
+    avatar?: string
     rpcURL: string
     wssURL: string
     explorerURL: string | undefined
@@ -12,27 +13,30 @@ export default {
     0: {
         description: "Not Connected",
         name: "offline",
+        avatar: "/disconnected.png",
         rpcURL: "",
         wssURL: "",
-        explorerURL: "https://etherscan.io/tx"
+        explorerURL: "https://etherscan.io/tx",
     },
     1: {
         description: "Ethereum Mainnet",
-        name: "mainnet",
+        name: "Ethereum",
+        avatar: "/ethereum.png",
         rpcURL: "https://mainnet.infura.io/v3/",
         wssURL: "wss://mainnet.infura.io/ws/v3/",
-        explorerURL: "https://etherscan.io/tx"
+        explorerURL: "https://etherscan.io/tx",
     },
     3: {
         description: "Ropsten Testnet",
         name: "ropsten",
         rpcURL: "https://ropsten.infura.io/v3/",
         wssURL: "wss://ropsten.infura.io/ws/v3/",
-        explorerURL: "https://etherscan.io/tx"
+        explorerURL: "https://etherscan.io/tx",
     },
     369: {
         description: "PulseChain",
         name: "pulsechain",
+        avatar: "/pulsechain.png",
         rpcURL: "https://rpc.pulsechain.com",
         wssURL: "wss://rpc.pulsechain.com",
         explorerURL: "https://scan.pulsechain.com/",
